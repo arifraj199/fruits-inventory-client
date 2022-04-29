f418cf4c5c2a7248c63a2ca8b78cb352f704920d
arifraj199 <karif9514@gmail.com>
2022-04-29 22:38:26 +0600

    simple banner added on homepage

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,8 +3,12 @@ import "./Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h2>This is home page</h2>
+    <div className="banner-container">
+      <div className="banner-title">
+          <h1>Organic Fruits & Vegetable <br /> For Live Healthy</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut enim sint laboriosam error. Eveniet sed illum ut perferendis dicta, voluptatem, quibusdam ipsam atque impedit cum delectus, aspernatur distinctio at nihil.</p>
+          <button className="btn btn-outline-success fs-5 text-white fw-bold">Know More</button>
+      </div>
     </div>
   );
 };
