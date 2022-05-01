8e292563f2621bd7a71fc0fc5a4930f22e356c9f
arifraj199 <karif9514@gmail.com>
2022-05-02 01:15:34 +0600

    login page reset email link implement

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../../firebase.init";
 import "./Login.css";
@@ -15,14 +15,20 @@ const Login = () => {
   ] = useSignInWithEmailAndPassword(auth);
 
   const [signInWithGoogle, user1, loading1, error1] = useSignInWithGoogle(auth);
+
+  const [sendPasswordResetEmail, sending, error2] = useSendPasswordResetEmail(
+    auth
+  );
+
   const location = useLocation();
   const navigate = useNavigate();
   let errorElement;
+  const emailRef = useRef('');
 
   const from = location.state?.from?.pathname || '/';
 
-  if(error || error1){
-    errorElement = <p>Error: {error?.message} {error1?.message}</p>
+  if(error || error1 || error2){
+    errorElement = <p>Error: {error?.message} {error1?.message} {error2?.message}</p>
   }
 
   if(user || user1){
@@ -39,13 +45,25 @@ const Login = () => {
     
   }
 
+  const handleReset = async ()=> {
+    const email = emailRef.current.value
+    // console.log(email);
+    if(!email){
+      alert('please enter your email for reset')
+      
+    }else{
+      await sendPasswordResetEmail(email);
+      alert('send email for reset password');
+    }
+  }
+
   return (
     <div className="w-25 mx-auto my-5 border border-2 p-4">
       <h2 className="text-center mb-4">Login</h2>
       <Form onSubmit={handleLoginForm}> 
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter email" />
+          <Form.Control type="email" ref={emailRef} name="email" placeholder="Enter email" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -59,6 +77,8 @@ const Login = () => {
 
       <p className="text-danger">{errorElement}</p>
       <p><small>Don't have any account? <span><Link className="text-decoration-none" to='/register'>Create Account</Link></span></small></p>
+
+      <p><small>Forget Your Password? <span><Link onClick={handleReset} className="text-decoration-none" to='/login'>Reset</Link></span></small></p>
       <p className="text-center mt-3">Or</p>
       <div>
       <button onClick={()=> signInWithGoogle()} className="btn text-center w-100 rounded-3 justify-content-center align-items-center mx-auto d-flex btn-info fw-bold">
