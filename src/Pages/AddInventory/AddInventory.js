b580847d030ebcc066397a37589a8098fda57828
arifraj199 <karif9514@gmail.com>
2022-05-01 11:55:43 +0600

    add items page functionality added and data send to the server

--- src/Pages/AddInventory/AddInventory.js
+++ src/Pages/AddInventory/AddInventory.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const AddInventory = () => {
-    return (
-        <div>
-            <h2>Add Inventory</h2>
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => {
+      console.log(data);
+      
+      //send data to the server
+      const url = `http://localhost:5000/inventory`;
+      fetch(url,{
+          method:"POST",
+          headers:{
+              "content-type":"application/json"
+          },
+          body:JSON.stringify(data)
+      })
+      .then(res=>res.json())
+      .then(result=>{
+          console.log(result);
+      })
+    };
+
+  return (
+    <div>
+        <h2 className="text-center mt-3">Add New Items</h2>
+      <form
+        className="d-flex flex-column border border-2 rounded-3 p-5 w-25 mx-auto mt-3 mb-5"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          className="mb-2" placeholder="items name"
+          {...register("name", { required: true, maxLength: 20 })}
+        />
+        <input
+          className="mb-2" placeholder="items price" type="number"
+          {...register("price", { min: 5, max: 150 })}
+        />
+        <input
+          className="mb-2" 
+          placeholder="items quantity"
+          type="number"
+          {...register("quantity", { min: 5, max: 99 })}
+        />
+        <input
+          className="mb-2" placeholder="items photo url"
+          {...register("picture")}
+        />
+        <textarea
+          className="mb-2" placeholder="short description"
+          {...register("description", { required: true, maxLength: 150 })}
+        />
+        <input className="mb-2 btn btn-success" type="submit" value="Add Item" />
+      </form>
+    </div>
+  );
 };
 
 export default AddInventory;
