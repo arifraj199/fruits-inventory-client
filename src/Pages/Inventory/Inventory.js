c23422230f2408df65fc218d2f1c7ed5501b9d63
arifraj199 <karif9514@gmail.com>
2022-05-06 12:41:32 +0600

    inventory update page design implement

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -65,47 +65,58 @@ const Inventory = () => {
     }
 
   return (
-    <div>
+    <div className="inventory-product-container">
       <div className="product-div">
         <div className="product-img">
           <img src={product.picture} alt="" />
         </div>
         <div className="product-detail">
-          <p>
-            Products Id: {id}
-          </p>
           <h1>
             {product.name}
           </h1>
+
           <h4>
             Price: {product.price}
           </h4>
+          <hr />
+          <div className="mt-2 mb-4">
+            <p>
+              <small className="fw-bold">
+                Quick Overview
+              </small>
+            </p>
+            <p>
+              <small>
+                {product.description}
+              </small>
+            </p> 
+          </div>
           <p>
-            <span className="fw-bold">Quantity :</span> <input className="border border-1" type="text"  value={product.quantity} readOnly/>
+            <span className="fw-bold">Quantity :</span> <input className="border border-0 bg-light" type="text"  value={product.quantity} readOnly/>
           </p>
+
           <p>
             <small>
-              {product.description}
-            </small>
-          </p> 
-          <p>Stock : {product.quantity <= 0 ? <span className="fw-bold text-danger">Stock Out</span> :<span className="fw-bold">Available</span>}</p>
-          <p>
-            <small>
-              Supplier Name: {product.supplier_name}
+              <span className="fw-bold">Supplier Name: </span>{product.supplier_name}
             </small>
           </p>
+          
+          <p><small><span className="fw-bold">Stock : </span>{product.quantity <= 0 ? <span className="fw-bold text-danger">Stock Out</span> :<span >Available</span>}</small></p>
+          
           <div className="button-field">
-            <button onClick={handleDeliver} className="btn btn-success">Delivered</button>
+            <button onClick={handleDeliver} className="btn ">Delivered</button>
             <div className="d-flex">
-              <input ref={restockRef} type="number" name="restockNumber"  />
-              <button onClick={handleRestockButton} className="btn btn-success ms-1"> Restock</button>
+              <input className="border border-0 bg-light" ref={restockRef} type="number" name="restockNumber" placeholder="restoke item"  />
+              <button onClick={handleRestockButton} className="btn ms-1"> Restock</button>
             </div>
           </div>
           
         </div>
+        
       </div>
-      <div className="manage-inventory my-5 text-end">
-        <Link to='/manageitem'><button className="btn btn-success me-1">Manage Inventories</button></Link>
+      
+      <div className="manage-inventory text-end">
+        <Link to='/manageitem'><button className="btn btn-success ">Manage Inventories</button></Link>
       </div>
     </div>
   );
